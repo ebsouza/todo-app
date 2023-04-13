8cce88c5b633b931467a847c8ec0a9928cdb6715
ebsouza <ebsouza07@yahoo.com>
2023-04-13 14:20:00 -0300

    test: update get all test

--- tasks/handler_test.go
+++ tasks/handler_test.go
@@ -1,24 +1,25 @@
 package tasks
 
 import (
-	"net/http"
+	"bytes"
 	"encoding/json"
-	"gorm.io/gorm"
-	"net/http/httptest"
-	"testing"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/sqlite"
-	"bytes"
-	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
 )
 
 type TaskPostRequest struct {
-	title string
+	title       string
 	description string
 }
 
-
 func setupTestRouters() (*gin.Engine, *repository) {
+	os.Remove("test.db")
 	dbHandler, _ := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	repository := NewRepository(dbHandler)
 	router := InitializeRouter(repository)
@@ -30,13 +31,13 @@ func TestCreateTask(t *testing.T) {
 	router, _ := setupTestRouters()
 
 	data := map[string]string{
-        "title": "X",
-        "description": "Y",
-		"status": "OK",
-    }
+		"title":       "X",
+		"description": "Y",
+		"status":      "OK",
+	}
 
 	body, _ := json.Marshal(data)
-    payload := bytes.NewBuffer(body)
+	payload := bytes.NewBuffer(body)
 	req, _ := http.NewRequest("POST", "/tasks/", payload)
 
 	w := httptest.NewRecorder()
@@ -51,29 +52,26 @@ func TestCreateTask(t *testing.T) {
 	assert.Equal(t, data["status"], task.Status)
 }
 
-
-func TestGetTask(t *testing.T) {
+func TestGetTasks(t *testing.T) {
 	router, repository := setupTestRouters()
+	var numberOfTasks int = 3
 
-	task := Task{Title: "X", Description: "Y", Status: "Z"}
-
-	repository.AddTask(&task)
+	for i := 0; i < numberOfTasks; i++ {
+		task := Task{Title: "A", Description: "B", Status: "C"}
+		repository.AddTask(&task)
+	}
 
 	req, _ := http.NewRequest("GET", "/tasks/", nil)
 
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	//var task Task
-	//json.Unmarshal(w.Body.Bytes(), &task)
+	var tasks []Task
+	json.Unmarshal(w.Body.Bytes(), &tasks)
 
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "200", w.Body.String())
-	//assert.Equal(t, data["title"], task.Title)
-	//assert.Equal(t, data["description"], task.Description)
-	//assert.Equal(t, data["status"], task.Status)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, numberOfTasks, len(tasks))
 }
-
 
 //https://github.com/learning-go-book/test_examples/tree/master/solver
 //https://gin-gonic.com/docs/testing/
